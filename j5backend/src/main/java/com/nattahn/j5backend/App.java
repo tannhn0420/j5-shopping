bd876e6e83ce90d12e98a3a46157b20a4d755ba1
tannhn0420 <tannhn0420@gmail.com>
2021-06-10 02:05:33 +0700

    Part 01

---
+++ j5backend/src/main/java/com/nattahn/j5backend/App.java
@@ -0,0 +1,13 @@
+package com.nattahn.j5backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
